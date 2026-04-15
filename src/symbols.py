f0b93adc6067bbccfb9bb747abae3c37d3e95b8a
pannet1 <prog@ecomsense.in>
2026-04-15 20:50:34 +0530

    simplify expiry sort to reverse=True

--- src/symbols.py
+++ src/symbols.py
@@ -146,7 +146,7 @@ def find_expiry_from_base(basename):
         df = pd.read_csv(file_path)
         formatted = df["expiry"].astype(str)
         expiries = formatted.unique().tolist()
-        expiries.sort(key=lambda x: x.split('-')[0] + x.split('-')[1] + x.split('-')[2], reverse=True)
+        expiries.sort(reverse=True)
         return expiries
     except Exception as e:
         logging.error(f"{e} when finding expiry from base")
