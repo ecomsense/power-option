eff32597cbf0f78a7daf44dd654155d2be361b5c
pannet1 <prog@ecomsense.in>
2026-04-16 05:42:12 +0530

    add sudo for nginx

--- factory/tail_logs.sh
+++ factory/tail_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Create tmux with 3 panes: app, nginx, gunicorn
 tmux new-session -d -s logs 'tail -f ~/power-option/data/log.txt'
-tmux split-window -h 'tail -f /var/log/nginx/access.log'
+tmux split-window -h 'sudo tail -f /var/log/nginx/access.log'
 tmux select-pane -t 0
 tmux split-window -v 'journalctl -u fastapi_app -f --no-pager'
 tmux select-layout -t logs tiled
