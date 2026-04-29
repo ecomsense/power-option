57be61dcd0535b812df5032bcceeeb738858062f
pannet1 <prog@ecomsense.in>
2026-04-30 02:26:28 +0530

    test: change start time to 02:30 for immediate test

--- src/main.py
+++ src/main.py
@@ -102,8 +102,8 @@ def load_page_template(name: str) -> str:
 class ScheduleConfig:
     def __init__(self):
         self.enabled = True
-        self.start_hour = 14
-        self.start_minute = 27
+        self.start_hour = 2
+        self.start_minute = 30
         self.end_hour = 15
         self.end_minute = 31
         self.trading_days = [0, 1, 2, 3, 4]
